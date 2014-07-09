c5d954814221847c9c38001428d598b8a498b668
Eric Mill <eric.mill@gsa.gov>
2014-07-08 22:10:51 -0400

    getting some python requirements in

--- state/state_data.py
+++ state/state_data.py
@@ -13,23 +13,26 @@ def run(options):
   pass
 
 
-# read options from the command line
-#   e.g. ./state.py --since=2012-03-04 --debug
-#     => {"since": "2012-03-04", "debug": True}
-def options():
-  options = {}
-  for arg in sys.argv[1:]:
-    if arg.startswith("--"):
 
-      if "=" in arg:
-        key, value = arg.split('=')
-      else:
-        key, value = arg, "True"
 
-      key = key.split("--")[1]
-      if value.lower() == 'true': value = True
-      elif value.lower() == 'false': value = False
-      options[key.lower()] = value
-  return options
+
+# make a unique ID out of the document's URL path
+# cause that's all we really got for unique IDs here
+# examples:
+#   "DOCUMENTS\\IntAgreements\\0000CD01.pdf"
+#   "DOCUMENTS/5-FY2014/F-2011-03391/DOC_0C17693711/C17693711.pdf"
+#
+# should yield something like "DOCUMENTS-IntAgreements-0000CD01"
+def document_id_for(filename):
+  original_path = original_path.replace("\\", "/")
+  name, ext = os.path.splitext(original_path)[0]
+  return name.replace("/", "-")
+
+# figure out the URL that a filename should generate
+# examples:
+#   "DOCUMENTS\\IntAgreements\\0000CD01.pdf"
+#   "DOCUMENTS/5-FY2014/F-2011-03391/DOC_0C17693711/C17693711.pdf"
+def url_for(original_path):
+  return "http://foia.state.gov/searchapp/" + original_path.replace("\\", "/")
 
 run(options())
