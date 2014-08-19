05beeddcf6b36b98601f7b451cf4c969d596cac4
Eric Mill <eric.mill@gsa.gov>
2014-08-19 19:27:10 -0400

    session preservation and posting works

--- documents/tasks/foiaonline.py
+++ documents/tasks/foiaonline.py
@@ -3,7 +3,9 @@
 import sys
 import os
 import utils
+import requests
 import json
+from bs4 import BeautifulSoup
 
 # options:
 #   page: only do a particular page number
@@ -15,7 +17,41 @@ import json
 #   data: override data directory (defaults to "./data")
 
 def run(options):
-	pass
+
+  session = new_session()
+  print(search("the", session))
+
+
+# will initialize a requests session, and stick some hidden params onto it
+def new_session():
+  session = requests.Session()
+  # first, GET the search form to fetch the _sourcePage CSRF param
+  search_url = "https://foiaonline.regulations.gov/foia/action/public/search/"
+  search_response = session.get(search_url)
+
+  search_doc = BeautifulSoup(search_response.content)
+  session.__sourcePage = search_doc.select("input[name=_sourcePage]")[0]['value']
+  session.__fp = search_doc.select("input[name=__fp]")[0]['value']
+
+  return session
+
+# given a session, return the raw HTML when searching for a term.
+def search(term, session):
+
+  base_url = "https://foiaonline.regulations.gov/foia/action/public/search//runSearch"
+
+  post_params = {
+    "searchParams.searchTerm": term,
+    "searchParams.forRequest": "true",
+    "searchParams.forAppeal": "true",
+    "searchParams.forRecord": "true",
+    "_sourcePage": session.__sourcePage,
+    "__fp": session.__fp
+  }
+
+  response = session.post(base_url, data=post_params)
+
+  return response.content
 
 
 run(utils.options()) if (__name__ == "__main__") else None
