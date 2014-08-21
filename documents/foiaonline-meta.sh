9d6c1e311a26f23a1b9e8f018198106ee0ac64e0
Eric Mill <eric.mill@gsa.gov>
2014-08-21 10:53:31 -0400

    helper script to archive metadata for foiaonline

---
+++ documents/foiaonline-meta.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+python tasks/foiaonline.py --term=epa
+python tasks/foiaonline.py --term=cbp
+python tasks/foiaonline.py --term=doc
+python tasks/foiaonline.py --term=mspb
+python tasks/foiaonline.py --term=flra
+python tasks/foiaonline.py --term=nara
+python tasks/foiaonline.py --term=pbgc
+python tasks/foiaonline.py --term=don
