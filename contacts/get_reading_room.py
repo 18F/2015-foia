25606ee61bd3333b28ae01bba7782dd4e4bb02c1
Shashank Khandelwal <shashank.khandelwal@gsa.gov>
2014-11-10 19:01:58 +0000

    Clean link names, return None instead of empty list, ignore certifications

--- contacts/get_reading_room.py
+++ contacts/get_reading_room.py
@@ -7,7 +7,7 @@ import yaml
 from glob import glob
 from bs4 import BeautifulSoup
 
-from scraper import agency_yaml_filename
+from scraper import agency_yaml_filename, AGENCIES
 
 def read_yaml_file(agency_abbr):
     yaml_filename = agency_yaml_filename('data', agency_abbr)
@@ -24,6 +24,10 @@ def domains_match(website_url, reading_room_url):
     website_parsed = urlparse(website_url)
     reading_room_parsed = urlparse(reading_room_url)
     return website_parsed.netloc == reading_room_parsed.netloc
+
+
+def clean_link_text(link_text):
+    return link_text.strip().replace('\n', '').replace('\r', '')
         
 
 def get_absolute_url(link, url):
@@ -34,18 +38,22 @@ def get_absolute_url(link, url):
             if href == get_base_url(url):
                 return None
         if domains_match(url, href):
-            return (link.text.strip(), href)
+            return (clean_link_text(link.text), href)
 
 def unique_links(links):
     redirected = []
     for l in links:
-        response = requests.get(l[1])
-        if len(response.history) > 0:
-            if response.history[0].status_code == 301:
-                redirected.append(
-                    (l[0], response.history[0].headers['Location']))
-        else:
-            redirected.append(l)
+        try: 
+            response = requests.get(l[1], verify=False)
+            if len(response.history) > 0:
+                if response.history[0].status_code == 301:
+                    redirected.append(
+                        (l[0], response.history[0].headers['Location']))
+            else:
+                redirected.append(l)
+        except requests.exceptions.ConnectionError:
+            #Ignore the link, as it clearly doesn't work. 
+            pass
             
     seen = set()
     uniques = [l for l in redirected if l[1] not in seen and not seen.add(l[1])]
@@ -58,6 +66,8 @@ def process(data):
         except requests.exceptions.MissingSchema:
             with_schema = 'http://%s' % data['website']
             response = requests.get(with_schema, verify=False)
+        except:
+            return None
 
         if response.status_code == 200:
             doc = BeautifulSoup(response.content)
@@ -65,28 +75,31 @@ def process(data):
             counter = 0
             links = set()
             for link in all_as:
-                for keyword in ['foia library', 'freedom of information library', 'reading room', 'vault']:
-                    if keyword in link.text.lower():
+                for keyword in [
+                    'foia library',
+                    'freedom of information library', 'reading room', 'vault']:
+                    if keyword in link.text.lower() and 'certification' not in link.text.lower():
                         url_pair = get_absolute_url(link, data['website'])
                         if url_pair:
                             links.add(url_pair)
             links = unique_links(links)
-            if len(links) > 1:
-                print(data['website'])
-                print(len(links))
-                print(links)
-                print('--------------')
-            
+            if len(links) == 0:
+                return None
+            return links
         
 
 def reading_room(agency_abbr):
     agency_data = read_yaml_file(agency_abbr)
     if agency_data:
         process(agency_data)
-
         if 'departments' in agency_data:
             for department in agency_data['departments']:
-                process(department)
+                return process(department)
+
+
+def all_reading_rooms():
+    for agency in AGENCIES:
+        reading_room(agency)
         
 
 if __name__ == "__main__":
@@ -96,3 +109,5 @@ if __name__ == "__main__":
 
     if agency_abbr:
         reading_room(agency_abbr)
+    else:
+        all_reading_rooms() 
